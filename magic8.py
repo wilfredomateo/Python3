8191327d3c4ad2148ea1fee856353b79df1b6259
wilfredomateo <wmateo86@gmail.com>
2022-11-29 19:38:20 -0600

    first python project

---
+++ magic8.py
@@ -0,0 +1,38 @@
+import random
+# A simple magic 8 ball program 
+
+name = "Wilfredo"
+question = "Am I the fairest of the land?"
+answer = ""
+
+# Generate the randomizer
+random_number = random.randint(1, 10)
+
+# Test the randomizer
+#print(random_number)
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+elif random_number == 10:
+  answer = "Signs point to yes"
+else:
+  answer = "Error"
+
+print(name + " asks: " + question)
+print("Magic 8-Ball's answer: " + answer)
