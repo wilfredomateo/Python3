f159482929982ad15f8cef280fb0de769e537c53
wilfredomateo <wmateo86@gmail.com>
2023-01-04 19:47:54 -0600

    fixed logic

--- bexar_county_jail_activity/bexar_county_jail_activity.py
+++ bexar_county_jail_activity/bexar_county_jail_activity.py
@@ -3,14 +3,14 @@
 import csv
 
 with open('./reports/01Jan2023.txt', 'r') as input_file:
-    input_file.seek(673) # move to location of first data entry
-    line = input_file.readline()
-    list = line.split()
+    lines = input_file.readlines()
     with open('./reports/01Jan2023.csv', 'w') as output_file:
         writer = csv.writer(output_file)
-        writer.writerow(('Booking Number', 'Day', 'Month', 'Year', 'Time', 'Last Name', 'First Name', 'Middle I', 'Race', 'Gender', 'DOB', 'Address1', 'Address2', 'Address3', 'Address4', 'Address5', 'Address6'))
-        writer.writerow(list)
-        #print(list)
+        writer.writerow(('Booking Number', 'Day', 'Month', 'Year', 'Time', 'Last Name', 'First Name', 'Middle I', 'Race', 'Gender', 'DOB', 'Address'))
+        for line in lines:
+            if line.startswith('0B20'):
+                writer.writerow(line.split())
+        
 
 
 
