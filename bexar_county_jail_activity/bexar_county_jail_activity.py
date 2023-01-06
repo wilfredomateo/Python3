01ea0c56f24c781f43f7e09140ee1bb0471f6f89
wilfredomateo <wmateo86@gmail.com>
2023-01-05 18:25:34 -0600

    update

--- bexar_county_jail_activity/bexar_county_jail_activity.py
+++ bexar_county_jail_activity/bexar_county_jail_activity.py
@@ -2,14 +2,13 @@
 
 import csv
 
-with open('./reports/01Jan2023.txt', 'r') as input_file:
+with open('./reports/KB5.txt', 'r') as input_file:
     lines = input_file.readlines()
-    with open('./reports/01Jan2023.csv', 'w') as output_file:
+    with open('./reports/KB5.csv', 'w') as output_file:
         writer = csv.writer(output_file)
         writer.writerow(('Booking Number', 'Month', 'Day', 'Year', 'Time', 'Last Name', 'First Name', 'Middle I', 'Race', 'Gender', 'DOB', 'Address'))
         for line in lines:
-            if line.startswith('0B20'):
-                writer.writerow(line.split())
+            writer.writerow(line.split())
         
 
 
